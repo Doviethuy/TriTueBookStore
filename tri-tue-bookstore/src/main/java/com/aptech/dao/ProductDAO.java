559da95bc43a02087188b3ba67a628a431d94823
namtbb <namtbb@bkav.com>
2017-03-27 17:57:43 +0700

    lấy product trên server

--- tri-tue-bookstore/src/main/java/com/aptech/dao/ProductDAO.java
+++ tri-tue-bookstore/src/main/java/com/aptech/dao/ProductDAO.java
@@ -1,73 +1,67 @@
 package com.aptech.dao;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.text.DateFormat;
-import java.text.ParseException;
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.JestResult;
+import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.core.Search;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchType;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.aptech.model.Product;
-import com.aptech.model.User;
-import com.aptech.util.FileUtil;
+import com.aptech.util.Constant;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 @Repository
 public class ProductDAO {
-	final static String ELASTICSEARCH_INDEX = "product";
-	final static String ELASTICSEARCH_TYPE = "book";
-	final static String ELASTICSEARCH_CLUSTER = "tritue";
-	final static String ELASTICSEARCH_NODE = "bookstore";
+
 	@Autowired
 	private SessionFactory sessionFactory;
 
 	public ArrayList<Product> getAllProduct() {
-		Session session = this.sessionFactory.getCurrentSession();
-		return new ArrayList<Product>(session.createQuery("from Product").list());
-		/*TransportClient client = null;
-		ArrayList<Product> lstProducts = new ArrayList<Product>();
-		DateFormat sourceFormat = new SimpleDateFormat("dd/MM/yyyy");
+		ArrayList<Product> products = new ArrayList<Product>();
+		JestClient client = connectElastic();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		try {
-			client = connectElastic(ELASTICSEARCH_CLUSTER, ELASTICSEARCH_NODE);
-			SearchResponse response = client.prepareSearch(ELASTICSEARCH_INDEX).setTypes(ELASTICSEARCH_TYPE).setSearchType(SearchType.DFS_QUERY_THEN_FETCH).setQuery(QueryBuilders.matchAllQuery()).setFrom(0).setSize(10000).setExplain(true).execute().actionGet();
-			SearchHit[] results = response.getHits().getHits();
-			for (SearchHit item : results) {
-				Map<String, Object> result = item.getSource();
-				Product p = new Product();
-				p.setProId(Long.valueOf(result.get("proId").toString()));
-				p.setPrice(Long.valueOf(result.get("price").toString()));
-				p.setProName(result.get("proName").toString());
-				p.setImg(result.get("img").toString());
-				p.setDescription(result.get("description").toString());
-				p.setUserName(result.get("userName").toString());
-				p.setCateId(Integer.valueOf(result.get("cateId").toString()));
-				p.setQuantity(Integer.valueOf(result.get("quantity").toString()));
-				p.setCreateDate(sourceFormat.parse(result.get("createDate").toString()));
-				p.setModifyDate(sourceFormat.parse(result.get("modifyDate").toString()));
-				lstProducts.add(p);
+			SearchSourceBuilder builder = new SearchSourceBuilder();
+			builder.query(QueryBuilders.matchAllQuery()).from(0).size(10000);
+			Search search = new Search.Builder(builder.toString()).addIndex(Constant.ELASTICSEARCH_INDEX).addType(Constant.ELASTICSEARCH_TYPE).build();
+			JestResult result = client.execute(search);
+			JsonObject object = result.getJsonObject();
+			JsonArray arr = object.get("hits").getAsJsonObject().get("hits").getAsJsonArray();
+			for (JsonElement item : arr) {
+				JsonObject temp = item.getAsJsonObject().get("_source").getAsJsonObject();
+				Product product = new Product();
+				product.setProId(Long.valueOf(temp.get("proId").toString()));
+				product.setPrice(Long.valueOf(temp.get("price").toString()));
+				product.setProName(temp.get("proName").toString());
+				product.setImg(temp.get("img").toString());
+				product.setDescription(temp.get("description").toString());
+				product.setUserName(temp.get("userName").toString());
+				product.setCateId(Integer.valueOf(temp.get("cateId").toString()));
+				product.setQuantity(Integer.valueOf(temp.get("quantity").toString()));
+//				product.setCreateDate(sdf.parse(temp.get("createDate").toString()));
+//				product.setModifyDate(sdf.parse(temp.get("modifyDate").toString()));
+				products.add(product);
 			}
-		} catch (UnknownHostException | ParseException e) {
-			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println(e);
 		} finally {
 			if (client != null) {
-				client.close();
+				client.shutdownClient();
 			}
 		}
-		return lstProducts;*/
+		return products;
 	}
 
 	public Product getProduct(long proId) {
@@ -78,7 +72,6 @@ public class ProductDAO {
 	public Product addProduct(Product product) {
 		Session session = this.sessionFactory.getCurrentSession();
 		session.save(product);
-		createData(product);
 		return product;
 	}
 
@@ -110,48 +103,16 @@ public class ProductDAO {
 
 	public boolean isEnoughQuantity(long proId, int quantity) {
 		Product product = this.getProduct(proId);
-		if (product == null || product.getQuantity() < quantity) {
+		if (product == null || product.getQuantity() > quantity) {
 			return false;
 		}
 		return true;
 	}
 
-	public static TransportClient connectElastic(String cluster, String node) throws UnknownHostException {
-		Settings settings = Settings.builder().put("cluster.name", cluster).put("node.name", node).put("http.enabled", false).put("node.master", false).put("network.host", "127.0.0.1").put("transport.tcp.port", "9300").build();
-		TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
-		return client;
+	public static JestClient connectElastic() {
+		JestClientFactory factory = new JestClientFactory();
+		factory.setHttpClientConfig(new HttpClientConfig.Builder(Constant.ELASTICSEARCH_HOME).multiThreaded(true).build());
+		return factory.getObject();
 	}
 
-	public static Map<String, Object> putJsonProduct(Product product) {
-		Map<String, Object> jsonDocument = new HashMap<String, Object>();
-		String proNameEn = FileUtil.removeAccent(product.getProName());
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		jsonDocument.put("proId", product.getProId());
-		jsonDocument.put("proName", product.getProName());
-		jsonDocument.put("proNameEn", proNameEn);
-		jsonDocument.put("cateId", product.getCateId());
-		jsonDocument.put("quantity", product.getQuantity());
-		jsonDocument.put("img", product.getImg());
-		jsonDocument.put("price", product.getPrice());
-		jsonDocument.put("description", product.getDescription());
-		jsonDocument.put("createDate", sdf.format(product.getCreateDate()));
-		jsonDocument.put("modifyDate", sdf.format(product.getModifyDate()));
-		jsonDocument.put("userName", product.getUserName());
-		return jsonDocument;
-	}
-
-	public static void createData(Product item) {
-		TransportClient client = null;
-		try {
-			client = connectElastic(ELASTICSEARCH_CLUSTER, ELASTICSEARCH_NODE);
-			client.prepareIndex(ELASTICSEARCH_INDEX, ELASTICSEARCH_TYPE, String.valueOf(item.getProId())).setSource(putJsonProduct(item)).execute().actionGet();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} finally {
-			if (client != null) {
-				client.close();
-			}
-		}
-	}
 }
-
--- tri-tue-bookstore/src/main/java/com/aptech/util/Constant.java
+++ tri-tue-bookstore/src/main/java/com/aptech/util/Constant.java
@@ -7,4 +7,9 @@ public class Constant {
 	public static final String ROLE = "sessionUserRole";
 	public static final byte ADMIN_ROLE = 1;
 	public static final byte STAFF_ROLE = 0;
+	public final static String ELASTICSEARCH_INDEX = "product";
+	public final static String ELASTICSEARCH_TYPE = "book";
+	public final static String ELASTICSEARCH_CLUSTER = "tritue";
+	public final static String ELASTICSEARCH_NODE = "bookstore";
+	public final static String ELASTICSEARCH_HOME = "http://104.199.187.30:80";
 }
