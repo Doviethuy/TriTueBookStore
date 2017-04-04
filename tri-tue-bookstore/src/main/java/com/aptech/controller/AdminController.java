704a042a1b8fdb9bcdf3a4b2f92b2e2b613178b0
doviethuy <doviethuy17111991@gmail.com>
2017-04-04 14:18:21 +0700

    admin thong ke

--- tri-tue-bookstore/src/main/java/com/aptech/controller/AdminController.java
+++ tri-tue-bookstore/src/main/java/com/aptech/controller/AdminController.java
@@ -20,6 +20,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.aptech.model.Category;
+import com.aptech.model.Invoice;
+import com.aptech.model.InvoiceDetail;
 import com.aptech.model.Product;
 import com.aptech.model.User;
 import com.aptech.service.CategoryService;
@@ -45,12 +47,127 @@ public class AdminController {
 	InvoiceDetailService invoiceDetailService;
 	@Autowired
 	CategoryService categoryService;
-
+		
+	@SuppressWarnings("deprecation")
 	@RequestMapping("/admin/report/doanh-thu")
 	public String index(Model model, HttpServletRequest request) {
 		HttpSession session = request.getSession();
 		if (PermissionUtil.checkLogin(session)) {
 			if (PermissionUtil.checkAdminRole(session)) {
+				ArrayList<Category> lstCategory = categoryService.getAllCategory();
+				ArrayList<Product> lstProduct = productService.getAllProduct();
+				ArrayList<Invoice> lstInvoice = invoiceService.getAllInvoice();
+				ArrayList<InvoiceDetail> lstInvoiceDetail = invoiceDetailService.getAllInvoiceDetail();
+				request.setAttribute("lstCate", lstCategory);				
+				request.setAttribute("lstPro", lstProduct);				
+				request.setAttribute("lstInvoice", lstInvoice);				
+				request.setAttribute("lstInvoiceDetail", lstInvoiceDetail);
+				Date today = new Date();
+				int year = today.getYear();
+				int month = today.getMonth();
+				int[] arrInv = new int[32];
+				float[] arrAmt = new float[32];
+				float[] arrCateAmt = new float[lstCategory.size()];
+				arrInv[0]=0;
+				arrAmt[0]=0;
+				if(month!=0){
+					for (int i = 0; i < lstCategory.size(); i++) {
+						int cateId = lstCategory.get(i).getCateId();
+						arrCateAmt[i] = 0;
+						for (InvoiceDetail detail : lstInvoiceDetail) {
+							Product product = productService.getProduct(detail.getProId());
+							Date cDate = detail.getCreateDate();
+							if(cDate.getYear()==year&&cDate.getMonth()==month-1&&product.getCateId()==cateId){
+								arrCateAmt[i]+=detail.getAmount();
+							}
+						}						
+					}
+					for (int i = 1; i < 32; i++) {
+						int value=0;
+						float amount=0;
+						for (Invoice invoice : lstInvoice) {
+							Date cDate = invoice.getCreateDate();
+							if(cDate.getYear()==year&&cDate.getMonth()==month-1&&cDate.getDate()==i){
+								value+=1;
+								amount+=invoice.getAmount();
+							}
+						}
+						arrInv[i]=value;
+						arrAmt[i]=amount;
+					}
+					request.setAttribute("month", month);
+					request.setAttribute("year", year+1900);
+				}else{
+					for (int i = 0; i < lstCategory.size(); i++) {
+						int cateId = lstCategory.get(i).getCateId();
+						arrCateAmt[i] = 0;
+						for (InvoiceDetail detail : lstInvoiceDetail) {
+							Product product = productService.getProduct(detail.getProId());
+							Date cDate = detail.getCreateDate();
+							if(cDate.getYear()==year&&cDate.getMonth()==11&&product.getCateId()==cateId){
+								arrCateAmt[i]+=detail.getAmount();
+							}
+						}						
+					}
+					for (int i = 1; i < 32; i++) {
+						int value=0;
+						float amount=0;
+						for (Invoice invoice : lstInvoice) {
+							Date cDate = invoice.getCreateDate();
+							if(cDate.getYear()==year-1&&cDate.getMonth()==11&&cDate.getDate()==i){
+								value+=1;
+								amount+=invoice.getAmount();
+							}
+						}
+						arrInv[i]=value;
+						arrAmt[i]=amount;
+					}
+					request.setAttribute("month", 12);
+					request.setAttribute("year", year+1899);
+				}
+				for (int i = 1; i < 32; i++) {
+					arrInv[0]+=arrInv[i];
+					arrAmt[0]+=arrAmt[i];
+				}
+				request.setAttribute("arrInvoice", arrInv);
+				request.setAttribute("arrAmount", arrAmt);
+				request.setAttribute("arrCatAmt", arrCateAmt);
+				request.setAttribute("today", new SimpleDateFormat("'Ngày' dd 'tháng' MM HH:mm:ss yyyy").format(today));
+				int[] arrMonth= new int[6];
+				int[] arrYear= new int[6];
+				int[] arrInvTotal= new int[6];
+				float[] arrAmtTotal= new float[6];
+				for (int i = 0; i < 6; i++) {
+					if(month-i>0){
+						arrMonth[i]=month-i;
+						arrYear[i]=year+1900;
+						arrInvTotal[i]=0;
+						arrAmtTotal[i]=0;
+						for (Invoice invoice : lstInvoice) {
+							Date cDate = invoice.getCreateDate();
+							if(cDate.getYear()==year&&cDate.getMonth()==month-i-1){
+								arrInvTotal[i]+=1;
+								arrAmtTotal[i]+=invoice.getAmount();
+							}
+						}
+					}else{
+						arrMonth[i]=month-i+12;
+						arrYear[i]=year+1899;
+						arrInvTotal[i]=0;
+						arrAmtTotal[i]=0;
+						for (Invoice invoice : lstInvoice) {
+							Date cDate = invoice.getCreateDate();
+							if(cDate.getYear()==year-1&&cDate.getMonth()==month-i+11){
+								arrInvTotal[i]+=1;
+								arrAmtTotal[i]+=invoice.getAmount();
+							}
+						}
+					}
+				}
+				request.setAttribute("arrMonth", arrMonth);
+				request.setAttribute("arrYear", arrYear);
+				request.setAttribute("arrInvTotal", arrInvTotal);
+				request.setAttribute("arrAmtTotal", arrAmtTotal);
 				return "admin/view-quan-ly";
 			} else {
 				return "redirect:/";
